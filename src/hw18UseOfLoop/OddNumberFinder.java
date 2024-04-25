82aaa2bf518722b6e99c449828fa18d459650802
ahmaddewan32 <ahmaddewan32@gmail.com>
2024-04-24 22:05:39 -0400

    update

---
+++ src/hw18UseOfLoop/OddNumberFinder.java
@@ -0,0 +1,23 @@
+package hw18UseOfLoop;
+
+public class OddNumberFinder {
+
+	// find all the odd numbers from a range starting from -20 till 50
+	public static void main(String[] args) {
+
+		for (int i = -20; i <= 50; i++)
+			
+		{
+
+			if ((i %2)== 1) {
+
+				System.out.println("Odd number:" + i);
+			}
+			{
+
+			}
+
+		}
+
+	}
+}
